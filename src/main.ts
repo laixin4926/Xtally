56640bd5b496029404b9d93f206cf138659fb293
clx <1149302926@qq.com>
2022-03-15 20:44:21 +0800

    创建icon

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import router from './router'
 import store from './store'
 import Daohang from '@/components/Daohang.vue';
 import Layout from '@/components/Layout.vue';
+import Icon from '@/components/Icon.vue';
 
 Vue.config.productionTip = false
 Vue.component('Layout',Layout)
-
+Vue.component('Icon',Icon)
 Vue.component('Daohang',Daohang)
 new Vue({
   router:router,
