c020d282d97aa12ada122dcf6334a9a7e25f9a4a
clx <1149302926@qq.com>
2022-03-15 18:35:26 +0800

    money初步页面布局

--- src/components/Daohang.vue
+++ src/components/Daohang.vue
@@ -1,5 +1,6 @@
+import nav from '@/components/nav.vue';
 <template>
-    <div>
+    <div class="nav">
     <router-link to="/money">记账</router-link>
     |
     <router-link to="/labels">标签</router-link>
@@ -15,5 +16,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+ .nav{
+     border: 1px solid red;
+ }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,9 @@
 
 <template>
-    <div>
-        Money.vue
+    <div class="nav-wrapper">
+        <div class="content">
+        <p>Money.vue</p>
+        </div>
         <Daohang/>
     </div>
     
@@ -14,5 +16,14 @@ export default{
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper{
+        border: 1px solid green;
+        display:flex;
+        flex-direction:column;
+        height: 100vh;
+    }
+    .content{
+        overflow: auto;
+        flex-grow: 1;
+    }
 </style>
