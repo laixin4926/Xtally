1f144b37f9dab0509fe90910c5c6fb5c26ee64dc
clx <1149302926@qq.com>
2022-03-15 16:43:19 +0800

    创建了三个页面，可以导航

---
+++ src/views/Money.vue
@@ -0,0 +1,16 @@
+import Money from '@/views/Money.vue';
+<template>
+    <div>
+        Money.vue
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
