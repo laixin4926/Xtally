add2fa92d7e8d4fe960404818f91d7a8e4b2ba67
clx <1149302926@qq.com>
2022-03-15 21:14:41 +0800

    添加icon样式

--- src/components/Daohang.vue
+++ src/components/Daohang.vue
@@ -1,32 +1,43 @@
-
 <template>
-    <div class="nav">
-    <router-link to="/money">
-    <Icon name="money"/>
-    记账
+  <nav>
+    <router-link to="/money" class="item">
+      <Icon class="icon" name="money" />
+      记账
     </router-link>
-    |
-    <router-link to="/labels">
-    <Icon name="label"/>
-    标签
+    
+    <router-link to="/labels" class="item">
+      <Icon class="icon" name="label" />
+      标签
     </router-link>
-    |
-    <router-link to="/statistics">
-    <Icon name="statistics"/>
-    统计
+    
+    <router-link to="/statistics" class="item">
+      <Icon class="icon" name="statistics" />
+      统计
     </router-link>
-    </div>
-</template >
+  </nav>
+</template>
 
 <script lang="ts">
-    
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
- .nav{
-     border: 1px solid red;
- }
+nav {
+  
+    display: flex;
+    box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  > .item{
+      padding: 2px 0;
+      width: 33.33333%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    .icon{
+        width: 32px;
+        height: 32px;
+    }
+
+  }
+}
 </style>
