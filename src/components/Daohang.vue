4be11b2df81e7f041ca1896d49f6f93e19d23d6f
clx <1149302926@qq.com>
2022-03-15 22:07:31 +0800

    修复图标bug 加了正确的meta

--- src/components/Daohang.vue
+++ src/components/Daohang.vue
@@ -1,16 +1,16 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon class="icon" name="money" />
       记账
     </router-link>
     
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon class="icon" name="label" />
       标签
     </router-link>
     
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon class="icon" name="statistics" />
       统计
     </router-link>
@@ -37,7 +37,10 @@ nav {
         width: 32px;
         height: 32px;
     }
-
   }
+  > .item.selected{
+      background: rgb(197, 193, 193);
+  }
+
 }
 </style>
