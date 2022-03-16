7573987ec6ef03ef73e9468e4f7914d8cbadfcbf
clx <1149302926@qq.com>
2022-03-16 14:35:49 +0800

    修改了导航的顺序，加了money的html，修改了money的icon样式

--- src/components/Daohang.vue
+++ src/components/Daohang.vue
@@ -1,15 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item" active-class="selected">
-      <Icon class="icon" name="money" />
-      记账
-    </router-link>
-    
     <router-link to="/labels" class="item" active-class="selected">
       <Icon class="icon" name="label" />
       标签
     </router-link>
-    
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon class="icon" name="money" />
+      记账
+    </router-link>
+
     <router-link to="/statistics" class="item" active-class="selected">
       <Icon class="icon" name="statistics" />
       统计
@@ -23,24 +22,22 @@ export default {};
 
 <style lang="scss" scoped>
 nav {
-  
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  > .item {
+    padding: 2px 0;
+    width: 33.33333%;
     display: flex;
-    box-shadow: 0 0 3px rgba(0,0,0,0.25);
-  > .item{
-      padding: 2px 0;
-      width: 33.33333%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-    .icon{
-        width: 32px;
-        height: 32px;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .icon {
+      width: 32px;
+      height: 32px;
     }
   }
-  > .item.selected{
-      background: rgb(197, 193, 193);
+  > .item.selected {
+    background: rgb(197, 193, 193);
   }
-
 }
 </style>
