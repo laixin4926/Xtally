c3f81e06979cd71ca83b9f49111298f949bd2411
clx <1149302926@qq.com>
2022-03-18 20:07:01 +0800

    EditLabel回退和删除功能

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -6,7 +6,9 @@ type Tag = {
 type TagListModel = {
   data: Tag[];
   fetch: () => Tag[];
+  remove: (id: string) => boolean;
   create: (name: string) => "success" | "duplicated";
+  update: (id: string, name: string) => "success" | "not found" | "duplicated";
   save: () => void;
 };
 const tagListModel: TagListModel = {
@@ -25,6 +27,35 @@ const tagListModel: TagListModel = {
     this.data.push({ id: name, name: name });
     this.save();
     return "success";
+  },
+  update(id: string, name: string) {
+    const idList = this.data.map((item) => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.data.map((item) => item.name);
+      if (name.indexOf(name) >= 0) {
+        return "duplicated";
+      } else {
+        const tag = this.data.filter((item) => item.id === id)[0];
+        tag.id = tag.name = name;
+
+        this.save();
+        return "success";
+      }
+    } else {
+      return "not found";
+    }
+  },
+  remove(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    this.data.splice(index, 1);
+    this.save();
+    return true;
   },
   save() {
     window.localStorage.setItem(
