06bbb44c83bfbfc6e055a6289bd3c6e1a0ea0316
clx <1149302926@qq.com>
2022-03-29 18:28:30 +0800

    qrcode

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,12 @@ module.exports = {
     : '/',
   lintOnSave: false,
   chainWebpack:config=>{
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title= '小新记账'
+      return args
+    })
     const dir =path.resolve(__dirname,'src/assets/icons')
 
     config.module
@@ -14,6 +20,8 @@ module.exports = {
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'),[{plainSprite:true}])
     config.module.rule('svg').exclude.add(dir)
+   
+
 
   }
 }
